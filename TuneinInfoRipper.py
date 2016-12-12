16e9c2d7267eadc6d1a9cfbf0e253c458586d339
notanewbie <computerchipcity@aol.com>
2016-12-12 11:20:19 -0500

    Ported features from old version

--- TuneinInfoRipper.py
+++ TuneinInfoRipper.py
@@ -21,6 +21,28 @@ def getURL():
     genre_ = raw_file.split("itemprop=\"genre\" content=\"")
     Genre_ = genre_[1].split("\"/>")
     Genre = Genre_[0]
+    raw_title = re.findall(r"<title>(.*?) - Listen Online",raw_file);
+    try:
+        full_title = str(raw_title[0]);
+    except IndexError:
+        raw = re.findall(r"<title>Listen to (.*?) online</title>",raw_file);
+        try:
+            full_title = str(raw[0]);
+        except IndexError:
+            rawe = re.findall(r"<h3>Listen to (.*?) on your phone!</h3>",raw_file);
+        else:
+            title = "is non-existent";
+            full_title = url1;
+    else:
+        try:
+            short_title = re.findall(r"(.*?) -", full_title);
+            title = str(short_title[0]);
+        except IndexError:
+            short_title = "is non-existent";
+            title = "is non-existent";
+        else:
+            short_title = "is non-existent";
+            title = "is non-existent";
     print "Logo: " + Logo
     print "Description: " + Description
     print "Genre: " + Genre
@@ -65,8 +87,58 @@ def getURL():
         if i is not 0:
             open_file = urllib2.urlopen("http://stream.radiotime.com/listen.stream?streamIds=" + str(NNStreamid[0]));
             raw_file = open_file.read();
-            print raw_file
+            #print raw_file
             ID.append(raw_file)
+            key_content = raw_file
+            raw_stream_url = re.findall(r"Url\": \"(.*?)\"", key_content);
+            #bandwidth = re.findall(r"Bandwidth\":(.*?),", key_content);
+            isPlaylist = re.findall(r"HasPlaylist\":(.*?),", key_content);
+            codec = re.findall(r"MediaType\": \"(.*?)\",", key_content);
+            tipe = re.findall(r"Type\": \"(.*?)\"", key_content);
+            total = 0
+            for element in raw_stream_url:
+                total = total + 1
+            total = 0
+            if total > 1:
+                print "I found " + str(total) + " streams.";
+            else:
+                print "I found only one stream.";
+            for element in raw_stream_url:
+                reliability = key_content.split('"Reliability":')
+                reliability = re.findall(r"lity\":(.*?),", key_content);
+                bandwidth = re.findall(r"Bandwidth\":(.*?),", key_content);
+                print "== Stream =="
+                print "Stream stats:";
+                print "Full station name: " + full_title + ".";
+                print "Shortened station name " + title + ".";
+                try:
+                    print "Bandwidth: " + str(bandwidth[i - 2]) + " kilobytes per second."
+                except IndexError:
+                    print "Bandwidth currently unavailable."
+                try:
+                    print "Reliability: " + str(reliability[i - 1]) + "%"
+                except IndexError:
+                    print "Reliability data currently unavailable."
+                try:
+                    print "HasPlaylist: " + str(isPlaylist[i - 1]) + "."
+                except IndexError:
+                    print "HasPlayist data currently unavailable."
+                try:
+                    print "Stream codec: " + str(codec[i - 1]) + "."
+                except IndexError:
+                    print "Stream codec data currently unavailable."
+                try:
+                   print "This audio stream is " + tipe[i - 1].lower() + "."
+                except IndexError:
+                    print "Audio stream type is unavailable."
+                try:
+                    print "Pure streaming URL: " + str(raw_stream_url[i - 2]) + ".";
+                except IndexError:
+                    print "Stream URL unavailable."
+                print "Logo URL: " + Logo;
+                print "Description: " + Description
+                print "Genre: " + Genre
         i = i + 1
+    
     getURL()
 getURL()
